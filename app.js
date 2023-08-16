1c181af9de56e0daf3df63bc92a51c4db86693aa
IrinaVi <IrinaVi>
2023-08-16 19:48:13 +0100

    Add cards array and shuffle it

--- app.js
+++ app.js
@@ -1,6 +1,54 @@
 const cardArray = [
     {
         name: 'fries',
-        img: 
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheesburgers',
+        img: 'images/cheesburgers.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
+    },
+    {
+        name: 'fries',
+        img: 'images/fries.png',
+    },
+    {
+        name: 'cheesburgers',
+        img: 'images/cheesburgers.png',
+    },
+    {
+        name: 'hotdog',
+        img: 'images/hotdog.png',
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/ice-cream.png',
+    },
+    {
+        name: 'pizza',
+        img: 'images/pizza.png',
+    },
+    {
+        name: 'milkshake',
+        img: 'images/milkshake.png',
     }
 ]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+const gridDisplay = document.querySelector('#grid');
